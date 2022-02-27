d7b930f5c8e672fee7f4494dc053f3000aaeb811
Ferruh <ferruhcihan20@gmail.com>
2022-02-27 18:10:54 +0300

    feat:add List & ListItem components

--- client/src/home/index.tsx
+++ client/src/home/index.tsx
@@ -1,12 +1,18 @@
 import "./home.scss";
 import Navbar from "../components/navbar";
 import Featured from "../components/featured";
+import List from "../components/list";
 
 export default function Home() {
   return (
     <div className="home">
       <Navbar />
       <Featured />
+      <List />
+      <List />
+      <List />
+      <List />
+      <List />
     </div>
   );
 }
