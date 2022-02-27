bfd1ceb1ee9455b5d0134b4888158c75c4158ced
Ferruh <ferruhcihan20@gmail.com>
2022-02-27 18:25:56 +0300

    feat:add login page & complete client ui

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,4 +1,7 @@
+import Register from "./pages/register";
+import Login from "./pages/login";
 import Home from "./pages/home";
+import Watch from "./pages/watch";
 
 function App() {
   return <Home />;
