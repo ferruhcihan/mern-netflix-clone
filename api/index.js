17a51c07a5e4fdb0b0a7112e8ad5ac48234d7875
Ferruh <ferruhcihan20@gmail.com>
2022-02-28 03:46:58 +0300

    feat:add db connection

--- api/index.js
+++ api/index.js
@@ -1,6 +1,16 @@
 const express = require('express');
 const app = express();
 const port = 5000;
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
+
+dotenv.config();
+
+main().then(() => console.log('Connected to DB Successfully!')).catch(err => console.log(err));
+
+async function main() {
+  await mongoose.connect(process.env.MONGO_URL);
+}
 
 app.listen(port, () => {
     console.log(`Server listening on port ${port}!`)
