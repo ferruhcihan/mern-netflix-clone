d16eedabf605082ef6896607b817759b5809815d
Ferruh <ferruhcihan20@gmail.com>
2022-03-01 02:56:39 +0300

    feat:update index.js

--- api/index.js
+++ api/index.js
@@ -1,17 +1,24 @@
-const express = require('express');
+const express = require("express");
 const app = express();
 const port = 5000;
-const mongoose = require('mongoose');
-const dotenv = require('dotenv');
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+const authRoute = require("./routes/auth");
 
 dotenv.config();
 
-main().then(() => console.log('Connected to DB Successfully!')).catch(err => console.log(err));
+main()
+  .then(() => console.log("Connected to DB Successfully!"))
+  .catch((err) => console.log(err));
 
 async function main() {
   await mongoose.connect(process.env.MONGO_URL);
 }
 
+app.use(express.json());
+
+app.use("/api/auth", authRoute);
+
 app.listen(port, () => {
-    console.log(`Server listening on port ${port}!`)
-})
+  console.log(`Server listening on port ${port}!`);
+});
