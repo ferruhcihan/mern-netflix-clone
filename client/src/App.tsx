0742de919e5ce63a5772468ab009e46608e1c6bb
Ferruh <ferruhcihan20@gmail.com>
2022-03-09 22:11:16 +0300

    feat:update app.tsx

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -2,9 +2,41 @@ import Register from "./pages/register";
 import Login from "./pages/login";
 import Home from "./pages/home";
 import Watch from "./pages/watch";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Redirect,
+} from "react-router-dom";
 
 function App() {
-  return <Home />;
+  const user = false;
+  return (
+    <Router>
+      <Switch>
+        <Route exact path="/">
+          {user ? <Home /> : <Redirect to="/register" />}
+        </Route>
+        <Route path="/register">
+          {!user ? <Register /> : <Redirect to="/" />}
+        </Route>
+        <Route path="/login">{!user ? <Login /> : <Redirect to="/" />}</Route>
+        {user && (
+          <>
+            <Route path="/movies">
+              <Home />
+            </Route>
+            <Route path="/series">
+              <Home />
+            </Route>
+            <Route path="/watch">
+              <Watch />
+            </Route>
+          </>
+        )}
+      </Switch>
+    </Router>
+  );
 }
 
 export default App;
