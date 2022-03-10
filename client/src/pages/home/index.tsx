5eed3ba9bf2b765596fd64d78e686269f433e31c
Ferruh <ferruhcihan20@gmail.com>
2022-03-10 23:43:26 +0300

    feat:add fetch method to home page

--- client/src/pages/home/index.tsx
+++ client/src/pages/home/index.tsx
@@ -1,8 +1,9 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import "./home.scss";
 import Navbar from "../../components/navbar";
 import Featured from "../../components/featured";
 import List from "../../components/list";
+import axios from "axios";
 
 interface IHome {
   type?: string;
@@ -11,6 +12,20 @@ interface IHome {
 export default function Home({ type }: IHome) {
   const [lists, setLists] = useState([]);
   const [genre, setGenre] = useState(null);
+
+  useEffect(() => {
+    const getRandomLists = async () => {
+      try {
+        const res = await axios.get(
+          `lists${type ? "?type=" + type : ""}${genre ? "&genre=" + genre : ""}`
+        );
+        setLists(res.data);
+      } catch (err) {
+        console.log(err);
+      }
+    };
+    getRandomLists();
+  }, [type, genre]);
 
   return (
     <div className="home">
